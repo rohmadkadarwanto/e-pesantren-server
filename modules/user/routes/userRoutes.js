3f2e7ed845522972b631ad1fa83f9f3f0b9d75df
Rohmad Kadarwanto <55898570+pakdherahmad@users.noreply.github.com>
2023-12-07 13:56:09 +0700

    Update userRoutes.js

--- modules/user/routes/userRoutes.js
+++ modules/user/routes/userRoutes.js
@@ -7,6 +7,5 @@ router.get('/users', userController.getAllUsers);
 router.get('/users/:id', userController.getUserById);
 router.post('/users', userController.createUser);
 router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
