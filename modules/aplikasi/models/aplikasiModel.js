a8a127c99baee839014cb479ca949005de59c07c
Rohmad Kadarwanto <55898570+pakdherahmad@users.noreply.github.com>
2023-12-07 13:58:09 +0700

    Update aplikasiModel.js

--- modules/aplikasi/models/aplikasiModel.js
+++ modules/aplikasi/models/aplikasiModel.js
@@ -49,15 +49,3 @@ exports.updateAplikasi = (aplikasiId, updatedAplikasiData) => {
     });
   });
 };
-
-exports.deleteAplikasi = (aplikasiId) => {
-  return new Promise((resolve, reject) => {
-    db.query('DELETE FROM application WHERE id = ?', [aplikasiId], (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
