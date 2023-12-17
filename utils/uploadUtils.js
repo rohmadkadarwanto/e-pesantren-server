55ed26360a03d68e991afd0a10e2a8d810fbfeae
Rohmad Kadarwanto <55898570+pakdherahmad@users.noreply.github.com>
2023-12-17 11:20:14 +0700

    Add files via upload

---
+++ utils/uploadUtils.js
@@ -0,0 +1,36 @@
+// utils/uploadUtils.js
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type. Only JPEG, PNG, and GIF files are allowed.'), false);
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadPath = path.join(__dirname, '../uploads');
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
+    }
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const fileName = `${file.fieldname}-${Date.now()}${ext}`;
+    cb(null, fileName);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter
+});
+
+module.exports = upload;
